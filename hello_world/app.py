14bac21945c9cacadc1236f4df1c7b7734847365
Gabriel Nakamura <gabrielnakamura@Gabriels-MacBook-Pro.local>
2023-10-13 11:31:55 -0300

    Inclusão dos métodos CRUD ao RDS

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,113 @@
 import json
+import pymsql
+import logging
 
-# import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+getMethod = "GET"
+postMethod = "POST"
+putMethod = "PUT"
+deleteMethod = "DELETE"
+
+tasksPath = "/user_tasks"
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    httpMethod = event["httpMethod"]
+    path = event["path"]
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    if httpMethod == getMethod and path == tasksPath:
+        return get_tasks(event)
+    elif httpMethod == postMethod and path == tasksPath:
+        return create_task(event)
+    elif httpMethod == putMethod and path == tasksPath:
+        return update_task(event)
+    elif httpMethod == deleteMethod and path == tasksPath:
+        return delete_task(event)
+    else:
+        return build_response(404, "Not Found")
+    
+def get_tasks(event):
+    try:
+        user_tasks_list = []
+        cur = get_mysql_connection().cursor()
+        cur.execute('SELECT * FROM user_tasks')
+        user_tasks = cur.fetchall()
+        cur.close()
+        for i in range(len(user_tasks)):
+            user_task_obj = {
+                'id': user_tasks[i][0],
+                'user_id': user_tasks[i][1],
+                'description': user_tasks[i][2],
+                'status': user_tasks[i][3],
+                'date': user_tasks[i][4]
+        }
+        user_tasks_list.append(user_task_obj)
+        build_response(200, user_tasks_list)
+    except Exception as error:
+        logger.exception("Error: could not fetch tasks: {}".format(error))
+        raise Exception("Error: could not fetch tasks: {}".format(error))
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+def create_task(event):
+    try:
+        body = json.loads(event["body"])
+        pathPrameters = event["pathParameters"]
+        user_id = pathPrameters["user_id"]
+        description = body["description"]
+        status = body["status"]
+        date = body["date"]
+        conn = get_mysql_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO user_tasks (user_id, description, status, date) VALUES (%s, %s, %s, %s)', (user_id, description, status, date))
+        conn.commit()
+        cur.close()
+        build_response(200, "Task created successfully")
+    except Exception as error:
+        logger.exception("Error: could not create task: {}".format(error))
+        raise Exception("Error: could not create task: {}".format(error))
+    
+def update_task(event):
+    try:
+        body = json.loads(event["body"])
+        pathPrameters = event["pathParameters"]
+        user_id = pathPrameters["user_id"]
+        description = body["description"]
+        status = body["status"]
+        date = body["date"]
+        conn = get_mysql_connection()
+        cur = conn.cursor()
+        cur.execute('UPDATE user_tasks SET description = %s, status = %s, date = %s WHERE user_id = %s', (description, status, date, user_id))
+        conn.commit()
+        cur.close()
+        build_response(200, "Task updated successfully")
+    except Exception as error:
+        logger.exception("Error: could not update task: {}".format(error))
+        raise Exception("Error: could not update task: {}".format(error))
+    
+def delete_task(event):
+    try:
+        pathPrameters = event["pathParameters"]
+        user_id = pathPrameters["user_id"]
+        conn = get_mysql_connection()
+        cur = conn.cursor()
+        cur.execute('DELETE FROM user_tasks WHERE user_id = %s', (user_id))
+        conn.commit()
+        cur.close()
+        build_response(200, "Task deleted successfully")
+    except Exception as error:
+        logger.exception("Error: could not delete task: {}".format(error))
+        raise Exception("Error: could not delete task: {}".format(error))
 
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+def build_response(statusCode, body=None):
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "statusCode": statusCode,
+        "body": json.dumps(body)
     }
+
+def get_mysql_connection():
+    try:
+        conn = pymysql.connect(host='localhost', user='root', password='password', db='todo', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+        return conn
+    except Exception as error:
+        logger.exception("Error: connection to RDS could not be established {}".format(error))
+        raise Exception("Connection to RDS could not be established {}".format(error))
