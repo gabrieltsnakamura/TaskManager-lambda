2f5e69d144de1e557fb5684efbb5043d48a7f3eb
Gabriel Nakamura <gabrielnakamura@Gabriels-MacBook-Pro.local>
2023-10-14 04:53:09 -0300

    Ajuste no terraform e variáveis

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,3 @@
-variable "backend_s3_bucket" {
-  type        = string
-  description = "The name of the S3 bucket used for Terraform remote state"
-  default     = "taskmanager-bucket"
-}
-
-variable "backend_s3_key" {
-  type        = string
-  description = "The name of the S3 key used for Terraform remote state"
-  default     = "terraform-statefiles/taskManager-infra.tfstate"
-}
-
-variable "backend_s3_region" {
-  type        = string
-  description = "The name of the S3 region used for Terraform remote state"
-  default     = "sa-east-1"
-}
-
 variable "lambda_filename" {
   type        = string
   description = "The name of the lambda zip file"
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -1,4 +1,4 @@
 # Lambda configuration
 lambda_function_name = "task-manager-lambda"
-lambda_handler       = "task-manager-lambda.handler"
+lambda_handler       = "task_manager_lambda.lambda_handler"
 lambda_filename      = "lambda_deployment.zip"
