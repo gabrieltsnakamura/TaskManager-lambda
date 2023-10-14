f95b451426d5da25af73a219ef5eb2112d156027
Gabriel Nakamura <gabrielnakamura@Gabriels-MacBook-Pro.local>
2023-10-14 04:39:40 -0300

    Terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,34 @@
 variable "backend_s3_bucket" {
-  type = string
+  type        = string
   description = "The name of the S3 bucket used for Terraform remote state"
-  default = "taskmanager-bucket"
+  default     = "taskmanager-bucket"
 }
 
 variable "backend_s3_key" {
-  type = string
+  type        = string
   description = "The name of the S3 key used for Terraform remote state"
-  default = "terraform-statefiles/taskManager-infra.tfstate"
+  default     = "terraform-statefiles/taskManager-infra.tfstate"
 }
-  
+
 variable "backend_s3_region" {
-  type = string
+  type        = string
   description = "The name of the S3 region used for Terraform remote state"
-  default = "sa-east-1"
+  default     = "sa-east-1"
 }
 
 variable "lambda_filename" {
-  type = string
+  type        = string
   description = "The name of the lambda zip file"
 }
 
 variable "lambda_function_name" {
-  type = string
+  type        = string
   description = "The name of the lambda function"
 }
-  
+
 variable "lambda_handler" {
-  type = string
+  type        = string
   description = "The name of the lambda handler"
-  default = "task-manager-lambda.handler"
+  default     = "task-manager-lambda.handler"
 }
 
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -1,4 +1,4 @@
 # Lambda configuration
 lambda_function_name = "task-manager-lambda"
-lambda_handler = "task-manager-lambda.handler"
-lambda_filename = "lambda_deployment.zip"
+lambda_handler       = "task-manager-lambda.handler"
+lambda_filename      = "lambda_deployment.zip"
