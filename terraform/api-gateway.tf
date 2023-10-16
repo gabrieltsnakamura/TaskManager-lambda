2c7472e57b960c48ef96a9278bb6fa079d866e4f
Gabriel Nakamura <gabrielnakamura@Gabriels-MacBook-Pro.local>
2023-10-16 01:51:48 -0300

    Inclusão do stage prod

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -35,3 +35,16 @@ resource "aws_lambda_permission" "task_manager_lambda_permission" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.task_manager_api_gateway.execution_arn}/*/*/*"
 }
+
+# Create a deployment for the API
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  stage_name  = "prod"
+}
+
+# Create a stage for the deployment
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  stage_name = "prod"
+}
