fe89db4e2d83ee24d6011083d012392454ed56ea
Gabriel Nakamura <gabrielnakamura@Gabriels-MacBook-Pro.local>
2023-10-16 01:45:56 -0300

    Inclusão de Api Gateway

---
+++ terraform/api-gateway.tf
@@ -0,0 +1,37 @@
+# Create an API Gateway REST API
+resource "aws_api_gateway_rest_api" "task_manager_api_gateway" {
+  name = "taskmanager-api-gateway"
+}
+
+# Create a resource for the Lambda function
+resource "aws_api_gateway_resource" "task_manager_api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.task_manager_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.task_manager_api_gateway.root_resource_id
+  path_part   = "user_tasks"
+}
+
+# Create a method for the resource
+resource "aws_api_gateway_method" "task_manager_api_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.task_manager_api_gateway.id
+  resource_id   = aws_api_gateway_resource.task_manager_api_gateway_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# Create an integration for the method
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.task_manager_api_gateway.id
+  resource_id             = aws_api_gateway_resource.task_manager_api_gateway_resource.id
+  http_method             = aws_api_gateway_method.task_manager_api_gateway_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.task_manager_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "task_manager_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.task_manager_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.task_manager_api_gateway.execution_arn}/*/*/*"
+}
