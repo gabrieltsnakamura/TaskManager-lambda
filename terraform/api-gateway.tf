510a79a821708f839d2ea41f40bd2514b6724112
Gabriel Nakamura <gabrielnakamura@Gabriels-MacBook-Pro.local>
2023-10-16 10:20:21 -0300

    Rollback para rotas a parte

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -7,51 +7,34 @@ resource "aws_api_gateway_rest_api" "task_manager_api_gateway" {
 resource "aws_api_gateway_resource" "task_manager_api_gateway_resource" {
   rest_api_id = aws_api_gateway_rest_api.task_manager_api_gateway.id
   parent_id   = aws_api_gateway_rest_api.task_manager_api_gateway.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "user_tasks"
 }
 
 # Create a method for the resource
-resource "aws_api_gateway_method" "task_manager_api_gateway_method" {
+resource "aws_api_gateway_method" "task_manager_api_gateway_method_delete" {
   rest_api_id   = aws_api_gateway_rest_api.task_manager_api_gateway.id
   resource_id   = aws_api_gateway_resource.task_manager_api_gateway_resource.id
-  http_method   = "ANY"
+  http_method   = "DELETE"
   authorization = "NONE"
 }
 
 # Create an integration for the method
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "integration_delete" {
   rest_api_id             = aws_api_gateway_rest_api.task_manager_api_gateway.id
   resource_id             = aws_api_gateway_resource.task_manager_api_gateway_resource.id
-  http_method             = aws_api_gateway_method.task_manager_api_gateway_method.http_method
-  integration_http_method = "POST"
+  http_method             = aws_api_gateway_method.task_manager_api_gateway_method_delete.http_method
+  integration_http_method = "DELETE"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.task_manager_lambda1.invoke_arn
 }
 
-# Create a method for the resource
-resource "aws_api_gateway_method" "task_manager_api_gateway_method_root" {
-  rest_api_id   = aws_api_gateway_rest_api.task_manager_api_gateway.id
-  resource_id   = aws_api_gateway_resource.task_manager_api_gateway_resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-# Create an integration for the method
-resource "aws_api_gateway_integration" "integration_root" {
-  rest_api_id             = aws_api_gateway_rest_api.task_manager_api_gateway.id
-  resource_id             = aws_api_gateway_resource.task_manager_api_gateway_resource.id
-  http_method             = aws_api_gateway_method.task_manager_api_gateway_method_root.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.task_manager_lambda1.invoke_arn
-}
 
 resource "aws_lambda_permission" "task_manager_lambda_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.task_manager_lambda1.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.task_manager_api_gateway.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.task_manager_api_gateway.execution_arn}/*/*/*"
 }
 
 # Create a deployment for the API
